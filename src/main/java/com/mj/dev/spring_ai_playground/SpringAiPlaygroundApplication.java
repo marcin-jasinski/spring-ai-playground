3937484f248c0cfc8bc2951945db8c9c4a38d5c0
Marcin <31375967+marcin-jasinski@users.noreply.github.com>
2025-09-13 13:24:28 +0200

    Spring Boot project init

---
+++ src/main/java/com/mj/dev/spring_ai_playground/SpringAiPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.mj.dev.spring_ai_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiPlaygroundApplication.class, args);
+    }
+
+}
